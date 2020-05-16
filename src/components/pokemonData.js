953a01a29c7b660ff3999321937a532f7b132321
Manuel Durdevic <manko97@hotmail.com>
2020-05-16 12:00:28 +0200

    working on stats bars

--- src/components/pokemonData.js
+++ src/components/pokemonData.js
@@ -22,8 +22,8 @@ class PokeData extends Component {
             this.setState({sprites: res.data.sprites});
             this.setState({stats: res.data.stats});
             this.setState({types: res.data.types});
-            this.state.stats.map((stat) => {
-                console.log(stat.stat.name);
+            this.state.stats.map((i) => {
+                console.log(i);
             })
         })
         .catch((error) => {
@@ -36,16 +36,19 @@ class PokeData extends Component {
         const sprites = this.state.sprites;
         const stats = this.state.stats;
         const pokeType = this.state.types;
+        const progressBar = {
+            width: this.state.stats.stat.base_stat + "%",
+        }
 
         return (
-            <div>
+            <div className="pokeDetail">
                 <div className="row">
                     <div className="col text-center">
                         <h1>
                             {pokeData.name}
                         </h1>
                         <br />
-                        <img src={sprites.front_default} alt="peep" className="img-fluid w-100" />
+                        <img src={sprites.front_default} alt="peep" className="img-fluid w-50" />
                         <p>
                             {pokeType.map((type, index) => {
                                 return (
@@ -58,64 +61,38 @@ class PokeData extends Component {
                 <div className="row">
                     <div className="col-4">
                         <ul>
-                            <li>
-                                Speed
-                            </li>
-                            <li>
-                                Special-defense
-                            </li> 
-                            <li>
-                                Special-attack
-                            </li>
-                            <li>
-                                Defense
-                            </li>
-                            <li>
-                                Attack
-                            </li>
-                            <li>
-                                Hp
-                            </li>
+                            { stats ? 
+                                stats.map((stat, index) => {
+                                    return (
+                                        <li key={index}>
+                                            {stat.stat.name}
+                                        </li>
+                                    )
+                                })
+                                :
+                                <li>
+                                    -
+                                </li>
+                            }
                         </ul>
                     </div>
                     <div className="col">
                         <ul>
-                            <li>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                                <br />
-                            </li>
-                            <li>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="25" aria-valuemax="100"></div>
-                                </div>
-                                <br />
-                            </li> 
-                            <li>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="25" aria-valuemax="100"></div>
-                                </div>
-                                <br />
-                            </li>
-                            <li>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="25" aria-valuemax="100"></div>
-                                </div>
-                                <br />
-                            </li>
-                            <li>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="25" aria-valuemax="100"></div>
-                                </div>
-                                <br />
-                            </li>
-                            <li>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="25" aria-valuemax="100"></div>
-                                </div>
-                                <br />
-                            </li>
+                            { stats ? 
+                                stats.map((stat, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={progressBar} aria-valuenow={stat.base_stat} aria-valuemin={0} aria-valuemax={100}></div>
+                                            </div>
+                                        </li>
+                                    )
+                                })
+                                :
+                                <li>
+                                    -
+                                </li>
+                            }
                         </ul>
                     </div>
                 </div>
